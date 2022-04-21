b923f9926f80d3450f6ad1e9e24353448ca427e4
kevinbfry <kfry@stanford.edu>
2022-04-21 12:47:27 -0700

    tree fixes

--- spe/estimators.py
+++ spe/estimators.py
@@ -1,106 +1,114 @@
 import numpy as np
+from numpy.core.numeric import allclose
+import pandas as pd
+from scipy.linalg import toeplitz, block_diag
+from scipy.spatial.distance import cdist
 from itertools import product
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge, LinearRegression, Lasso
 from sklearn.model_selection import cross_validate, GroupKFold, KFold
 from sklearn.cluster import KMeans
 
-from sklearn.base import BaseEstimator, clone
+from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 from .data_generator import DataGen
 from .relaxed_lasso import RelaxedLasso
 
 
-def cb_isotropic(X,
-                 y,
-                 sigma=None,
-                 nboot=100,
-                 alpha=1.,
-                 model=LinearRegression(),
-                 est_risk=True):
+class CBIsotropic(object):
 
-  model = clone(model)
+  def _estimate(self,
+                X,
+                y,
+                sigma=None,
+                nboot=100,
+                alpha=1.,
+                model=LinearRegression(),
+                est_risk=True):
 
-  X = X
-  y = y
-  (n, p) = X.shape
+    X = X
+    y = y
+    (n, p) = X.shape
 
-  if sigma is None:
-    model.fit(X, y)
-    pred = model.predict(X)
-    sigma = np.sqrt(((y - pred)**2).mean()) # not sure how to get df for general models...
+    if sigma is None:
+      model.fit(X, y)
+      pred = model.predict(X)
+      sigma = np.sqrt(((y - pred)**2).mean()) # not sure how to get df for general models...
 
-  boot_ests = np.zeros(nboot)
+    boot_ests = np.zeros(nboot)
 
-  for b in np.arange(nboot):
-    eps = sigma * np.random.randn(n)
-    w = y + eps*np.sqrt(alpha)
-    wp = y - eps/np.sqrt(alpha)
+    for b in np.arange(nboot):
+      eps = sigma * np.random.randn(n)
+      w = y + eps*np.sqrt(alpha)
+      wp = y - eps/np.sqrt(alpha)
 
-    model.fit(X, w)
-    yhat = model.predict(X)
+      model.fit(X, w)
+      yhat = model.predict(X)
 
-    boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(eps**2)/alpha
+      boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(eps**2)/alpha
 
-  return boot_ests.mean()/n  + (sigma**2)*(alpha - (1+alpha)*est_risk), model
-
-  # return boot_ests.mean()/n - (sigma**2)*est_risk
-
-
-def cb(X, 
-       y, 
-       Chol_t=None, 
-       Chol_eps=None,
-       Theta=None,
-       nboot=100,
-       model=LinearRegression(),
-       est_risk=True):
-
-  model = clone(model)
-
-  X = X
-  y = y
-  (n, p) = X.shape
-
-  if Chol_eps is None:
-    Chol_eps = np.eye(n)
-    Sigma_eps = Chol_eps
-  else:
-    Sigma_eps = Chol_eps @ Chol_eps.T
-
-  Prec_eps = np.linalg.inv(Sigma_eps)
-
-  if Chol_t is None:
-    Chol_t = np.eye(n)
-    Sigma_t = np.eye(n)
-  else:
-    Sigma_t = Chol_t @ Chol_t.T
-
-  proj_t_eps = Sigma_t @ Prec_eps
-
-  if Theta is None:
-    Theta = np.eye(n)
-  Sigma_t_Theta = Sigma_t @ Theta
-  Sigma_eps_Theta = Sigma_eps @ Theta
-
-  boot_ests = np.zeros(nboot)
-
-  for b in np.arange(nboot):
-    eps = Chol_eps @ np.random.randn(n)
-    w = y + eps
-    regress_t_eps = proj_t_eps @ eps
-    wp = y - regress_t_eps
-
-    model.fit(X, w)
-    yhat = model.predict(X)
-
-    boot_ests[b] = np.sum((wp - yhat)**2) - (regress_t_eps.T.dot(Theta @ regress_t_eps)).sum()
-
-  return (boot_ests.mean() - np.diag(Sigma_t_Theta).sum()*est_risk + np.diag(Sigma_eps_Theta).sum()*(1 - est_risk)) / n, model
+    return boot_ests.mean()/n  + (sigma**2)*(alpha - (1+alpha)*est_risk)
+    # return boot_ests.mean()/n - (sigma**2)*est_risk
 
 
+class CB(object):
 
-def blur_linear(X, 
+  def _estimate(self,
+                X, 
+                y, 
+                Chol_t=None, 
+                Chol_eps=None,
+                Theta=None,
+                nboot=100,
+                model=LinearRegression(),
+                est_risk=True):
+
+    X = X
+    y = y
+    (n, p) = X.shape
+
+    if Chol_eps is None:
+      Chol_eps = np.eye(n)
+      Sigma_eps = Chol_eps
+    else:
+      Sigma_eps = Chol_eps @ Chol_eps.T
+    
+    Prec_eps = np.linalg.inv(Sigma_eps)
+
+    if Chol_t is None:
+      Chol_t = np.eye(n)
+      Sigma_t = np.eye(n)
+    else:
+      Sigma_t = Chol_t @ Chol_t.T
+
+    proj_t_eps = Sigma_t @ Prec_eps
+
+    if Theta is None:
+      Theta = np.eye(n)
+    Sigma_t_Theta = Sigma_t @ Theta
+    Sigma_eps_Theta = Sigma_eps @ Theta
+
+    boot_ests = np.zeros(nboot)
+
+    for b in np.arange(nboot):
+      eps = Chol_eps @ np.random.randn(n)
+      w = y + eps
+      regress_t_eps = proj_t_eps @ eps
+      wp = y - regress_t_eps
+
+      model.fit(X, w)
+      yhat = model.predict(X)
+
+      boot_ests[b] = np.sum((wp - yhat)**2) - (regress_t_eps.T.dot(Theta @ regress_t_eps)).sum()
+
+    return (boot_ests.mean() - np.diag(Sigma_t_Theta).sum()*est_risk + np.diag(Sigma_eps_Theta).sum()*(1 - est_risk)) / n
+
+
+class BlurLinear(object):
+  
+  def _estimate(self,
+                X, 
                 y, 
                 Chol_t=None, 
                 Chol_eps=None,
@@ -109,179 +117,185 @@ def blur_linear(X,
                 model=LinearRegression(),
                 est_risk=True):
 
-  model = clone(model)
+    X = X
+    y = y
+    (n, p) = X.shape
 
-  X = X
-  y = y
-  (n, p) = X.shape
+    if Chol_eps is None:
+      Chol_eps = np.eye(n)
+      Sigma_eps = Chol_eps
+    else:
+      Sigma_eps = Chol_eps @ Chol_eps.T
+    
+    Prec_eps = np.linalg.inv(Sigma_eps)
 
-  if Chol_eps is None:
-    Chol_eps = np.eye(n)
-    Sigma_eps = Chol_eps
-  else:
-    Sigma_eps = Chol_eps @ Chol_eps.T
+    if Chol_t is None:
+      Chol_t = np.eye(n)
+      Sigma_t = np.eye(n)
+    else:
+      Sigma_t = Chol_t @ Chol_t.T
 
-  Prec_eps = np.linalg.inv(Sigma_eps)
+    proj_t_eps = Sigma_t @ Prec_eps
 
-  if Chol_t is None:
-    Chol_t = np.eye(n)
-    Sigma_t = np.eye(n)
-  else:
-    Sigma_t = Chol_t @ Chol_t.T
+    # if Theta is None:
+    #   Theta = np.eye(n)
+    Sigma_t_Theta = Sigma_t# @ Theta
 
-  proj_t_eps = Sigma_t @ Prec_eps
+    P = X @ np.linalg.inv(X.T @ X) @ X.T
 
-  # if Theta is None:
-  #   Theta = np.eye(n)
-  Sigma_t_Theta = Sigma_t# @ Theta
+    boot_ests = np.zeros(nboot)
 
-  P = X @ np.linalg.inv(X.T @ X) @ X.T
+    for b in np.arange(nboot):
+      eps = Chol_eps @ np.random.randn(n)
+      w = y + eps
+      regress_t_eps = proj_t_eps @ eps
+      wp = y - regress_t_eps
 
-  boot_ests = np.zeros(nboot)
+      model.fit(X, w)
+      yhat = model.predict(X)
 
-  for b in np.arange(nboot):
-    eps = Chol_eps @ np.random.randn(n)
-    w = y + eps
-    regress_t_eps = proj_t_eps @ eps
-    wp = y - regress_t_eps
+      boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(regress_t_eps**2) - np.sum((P @ eps)**2)
 
-    model.fit(X, w)
-    yhat = model.predict(X)
-
-    boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(regress_t_eps**2) - np.sum((P @ eps)**2)
-
-  return (boot_ests.mean() - np.diag(Sigma_t_Theta).sum()*est_risk) / n, model
-
+    return (boot_ests.mean() - np.diag(Sigma_t_Theta).sum()*est_risk) / n
 
 
 ## only full refit for now
+class BlurLasso(object):
   
-def blur_lasso(X, 
-               y, 
-               Chol_t=None, 
-               Chol_eps=None,
-               nboot=1,
-               model=RelaxedLasso(),
-               type='full',
-               est_risk=True):
+  def _estimate(self,
+                X, 
+                y, 
+                Chol_t=None, 
+                Chol_eps=None,
+                nboot=1,
+                model=RelaxedLasso(),
+                rand_type='full',
+                est_risk=True):
 
-  model = clone(model)
+    X = X
+    y = y
+    (n, p) = X.shape
 
-  X = X
-  y = y
-  (n, p) = X.shape
+    full_rand = rand_type == 'full'
 
-  if Chol_eps is None:
-    Chol_eps = np.eye(n)
-    Sigma_eps = Chol_eps
-  else:
-    Sigma_eps = Chol_eps @ Chol_eps.T
+    if Chol_eps is None:
+      Chol_eps = np.eye(n)
+      Sigma_eps = Chol_eps
+    else:
+      Sigma_eps = Chol_eps @ Chol_eps.T
+    
+    Prec_eps = np.linalg.inv(Sigma_eps)
 
-  Prec_eps = np.linalg.inv(Sigma_eps)
+    if Chol_t is None:
+      Chol_t = np.eye(n)
+      Sigma_t = np.eye(n)
+    else:
+      Sigma_t = Chol_t @ Chol_t.T
 
-  if Chol_t is None:
-    Chol_t = np.eye(n)
-    Sigma_t = np.eye(n)
-  else:
+    proj_t_eps = Sigma_t @ Prec_eps
+
+    # if Theta is None:
+    #   Theta = np.eye(n)
+    Sigma_t_Theta = Sigma_t# @ Theta
+
+    Aperpinv = np.eye(proj_t_eps.shape[0]) + proj_t_eps
+    Aperp = np.linalg.inv(Aperpinv)
+
+    boot_ests = np.zeros(nboot)
+
+    for b in np.arange(nboot):
+      eps = Chol_eps @ np.random.randn(n)
+      self.w = w = y + eps
+      regress_t_eps = proj_t_eps @ eps
+      wp = y - regress_t_eps
+
+      if full_rand:
+        model.fit(X, w, y)
+      else:
+        model.fit(X, w, w)
+      yhat = model.predict(X)
+
+      XE = model.predXE_
+      P = XE @ np.linalg.inv(XE.T @ XE) @ XE.T
+      PAperp = P @ Aperp
+
+      # boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(regress_t_eps**2) \
+      #                 + 2*regress_t_eps.T.dot(PAperp.dot(regress_t_eps))
+
+      boot_ests[b] = np.sum((wp - yhat)**2)
+
+    t_epsinv_t = proj_t_eps @ Sigma_t
+
+    return (boot_ests.mean() 
+            + 2*np.diag((Sigma_t + t_epsinv_t) @ PAperp).sum() * full_rand
+            - np.diag(t_epsinv_t).sum()
+            - np.diag(Sigma_t_Theta).sum()*est_risk) / n
+
+
+class KFoldCV(object):
+
+  def _estimate(self, 
+                model, 
+                X, 
+                y, 
+                k=5):
+
+    self.kfcv_res = kfcv_res = cross_validate(model, X, y, 
+                                              scoring='neg_mean_squared_error', 
+                                              cv=KFold(k, shuffle=True), 
+                                              error_score='raise')
+    return -np.mean(kfcv_res['test_score'])
+
+
+class KMeansCV(object):
+
+  def _estimate(self, 
+                model, 
+                X, 
+                y, 
+                k=5):
+
+    groups = KMeans(n_clusters=k).fit(X).labels_
+    self.spcv_res = spcv_res = cross_validate(model, 
+                                              X, 
+                                              y, 
+                                              scoring='neg_mean_squared_error', 
+                                              cv=GroupKFold(k), 
+                                              groups=groups)
+
+    return -np.mean(spcv_res['test_score'])
+
+
+class TestSetEstimator(object):
+
+  def _estimate(self, 
+                model, 
+                X, 
+                y,
+                y_test,
+                Chol_t=None, 
+                Theta=None,
+                est_risk=True):
+
+    (n, p) = X.shape
+
+    if Chol_t is None:
+      Chol_t = np.eye(n)
+
     Sigma_t = Chol_t @ Chol_t.T
 
-  proj_t_eps = Sigma_t @ Prec_eps
+    if Theta is None:
+      Theta = np.eye(n)
 
-  # if Theta is None:
-  #   Theta = np.eye(n)
-  Sigma_t_Theta = Sigma_t# @ Theta
+    Sigma_t_Theta = Sigma_t @ Theta
 
-  Aperpinv = np.eye(proj_t_eps.shape[0]) + proj_t_eps
-  Aperp = np.linalg.inv(Aperpinv)
+    model.fit(X, y)
+    preds = model.predict(X)
+    sse = np.sum((y_test - preds)**2)
 
-  boot_ests = np.zeros(nboot)
+    # print(np.diag(Sigma_t_Theta).sum())
 
-  for b in np.arange(nboot):
-    eps = Chol_eps @ np.random.randn(n)
-    w = y + eps
-    regress_t_eps = proj_t_eps @ eps
-    wp = y - regress_t_eps
-
-    model.fit(X, w, y)
-    yhat = model.predict(X)
-
-    XE = model.predXE_
-    P = XE @ np.linalg.inv(XE.T @ XE) @ XE.T
-    PAperp = P @ Aperp
-
-    # boot_ests[b] = np.sum((wp - yhat)**2) - np.sum(regress_t_eps**2) \
-    #                 + 2*regress_t_eps.T.dot(PAperp.dot(regress_t_eps))
-
-    boot_ests[b] = np.sum((wp - yhat)**2) \
-                    - np.diag(proj_t_eps @ Sigma_t).sum() \
-                    + 2*np.diag(proj_t_eps @ Sigma_t @ PAperp).sum()
-
-  return (boot_ests.mean() 
-          + 2*np.diag(Sigma_t @ PAperp).sum() 
-          - np.diag(Sigma_t_Theta).sum()*est_risk) / n, model
-
-
-
-def kfoldcv(model, 
-            X, 
-            y, 
-            k=5):
-
-  model = clone(model)
-
-  kfcv_res = cross_validate(model, X, y, 
-                            scoring='neg_mean_squared_error', 
-                            cv=KFold(k, shuffle=True), 
-                            error_score='raise')
-  return -np.mean(kfcv_res['test_score']), model
-
-
-
-def kmeanscv(model, 
-             X, 
-             y, 
-             k=5):
-
-  groups = KMeans(n_clusters=k).fit(X).labels_
-  spcv_res = spcv_res = cross_validate(model, 
-                                            X, 
-                                            y, 
-                                            scoring='neg_mean_squared_error', 
-                                            cv=GroupKFold(k), 
-                                            groups=groups)
-
-  return -np.mean(spcv_res['test_score']), model
-
-
-
-def test_set_estimator(model, 
-                       X, 
-                       y,
-                       y_test,
-                       Chol_t=None, 
-                       Theta=None,
-                       est_risk=True):
-
-  (n, p) = X.shape
-
-  if Chol_t is None:
-    Chol_t = np.eye(n)
-
-  Sigma_t = Chol_t @ Chol_t.T
-
-  if Theta is None:
-    Theta = np.eye(n)
-
-  Sigma_t_Theta = Sigma_t @ Theta
-
-  model.fit(X, y)
-  preds = model.predict(X)
-  sse = np.sum((y_test - preds)**2)
-
-  # print(np.diag(Sigma_t_Theta).sum())
-
-  return (sse - np.diag(Sigma_t_Theta).sum()*est_risk) / n, model
-
+    return (sse - np.diag(Sigma_t_Theta).sum()*est_risk) / n
 
 
 
--- spe/mse_estimator.py
+++ spe/mse_estimator.py
@@ -10,7 +10,7 @@ from sklearn.base import clone
 
 from .data_generator import DataGen
 from .relaxed_lasso import RelaxedLasso
-from .estimators import kfoldcv, kmeanscv, test_set_estimator, cb, cb_isotropic, blur_linear, blur_lasso
+from .estimators import KFoldCV, KMeansCV, TestSetEstimator, CB, CBIsotropic, BlurLinear, BlurLasso
 from .tree import Tree, BlurTreeIID
 
 class ErrorComparer(object):
@@ -22,8 +22,6 @@ class ErrorComparer(object):
                 Chol_t=None, 
                 Theta=None,
                 est_risk=True):
-
-    model = clone(model)
 
     (n, p) = X.shape
 
@@ -61,9 +59,9 @@ class ErrorComparer(object):
     self.cb_err = np.zeros(niter)
     self.cbiso_err = np.zeros(niter)
 
-    test_est = test_set_estimator
-    cb_est = cb
-    cbiso_est = cb_isotropic()
+    test_est = TestSetEstimator()
+    cb_est = CB()
+    cbiso_est = CBIsotropic()
 
     gen_beta = X is None or beta is None
 
@@ -91,30 +89,30 @@ class ErrorComparer(object):
       y_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
       y_test_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
 
-      self.test_err[i] = test_est(model=model,
-                                  X=X, 
-                                  y=y, 
-                                  y_test=y_test, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  est_risk=est_risk)[0]
-      self.test_err_alpha[i] = test_est(model=model,
-                                        X=X, 
-                                        y=y_alpha, 
-                                        y_test=y_test_alpha, 
-                                        Chol_t=np.eye(n)*np.sqrt(1+alpha)*sigma, 
-                                        est_risk=est_risk)[0]
-      self.cb_err[i] = cb_est(X=X, 
-                              y=y, 
-                              Chol_t=np.eye(n)*sigma, 
-                              Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                              model=model,
-                              est_risk=est_risk)[0]
-      self.cbiso_err[i] = cbiso_est(X,
-                                    y,
-                                    sigma=sigma,
-                                    alpha=alpha,
-                                    model=model,
-                                    est_risk=est_risk)[0]
+      self.test_err[i] = test_est._estimate(model=model,
+                                            X=X, 
+                                            y=y, 
+                                            y_test=y_test, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            est_risk=est_risk)
+      self.test_err_alpha[i] = test_est._estimate(model=model,
+                                                  X=X, 
+                                                  y=y_alpha, 
+                                                  y_test=y_test_alpha, 
+                                                  Chol_t=np.eye(n)*np.sqrt(1+alpha)*sigma, 
+                                                  est_risk=est_risk)
+      self.cb_err[i] = cb_est._estimate(X=X, 
+                                        y=y, 
+                                        Chol_t=np.eye(n)*sigma, 
+                                        Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
+                                        model=model,
+                                        est_risk=est_risk)
+      self.cbiso_err[i] = cbiso_est._estimate(X,
+                                              y,
+                                              sigma=sigma,
+                                              alpha=alpha,
+                                              model=model,
+                                              est_risk=est_risk)
 
     return (self.test_err,
             self.test_err_alpha,
@@ -130,17 +128,18 @@ class ErrorComparer(object):
                      X=None,
                      beta=None,
                      model=Tree(),
+                     rand_type='full',
                      alpha=0.05,
                      est_risk=True):
 
 
     self.test_err = np.zeros(niter)
-    self.test_err_alpha = np.zeros(niter)
-    self.cb_err = np.zeros(niter)
+    # self.test_err_alpha = np.zeros(niter)
+    # self.cb_err = np.zeros(niter)
     self.blur_err = np.zeros(niter)
 
-    test_est = test_set_estimator
-    cb_est = cb
+    test_est = TestSetEstimator()
+    cb_est = CB()
     blur_est = BlurTreeIID()
 
     gen_beta = X is None or beta is None
@@ -166,37 +165,27 @@ class ErrorComparer(object):
 
       y = mu + sigma * np.random.randn(n)
       y_test = mu + sigma * np.random.randn(n)
-      y_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
-      y_test_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
 
-      self.test_err[i] = test_est(model=model,
-                                  X=X, 
-                                  y=y, 
-                                  y_test=y_test, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  est_risk=est_risk)[0]
-      self.test_err_alpha[i] = test_est(model=model,
-                                        X=X, 
-                                        y=y_alpha, 
-                                        y_test=y_test_alpha, 
-                                        Chol_t=np.eye(n)*np.sqrt(1+alpha)*sigma, 
-                                        est_risk=est_risk)[0]
-      self.cb_err[i] = cb_est(X=X, 
-                              y=y, 
-                              Chol_t=np.eye(n)*sigma, 
-                              Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                              model=model,
-                              est_risk=est_risk)[0]
-      self.blur_err[i] = blur_est(X, 
-                                  y, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                                  model=model,
-                                  est_risk=est_risk)[0]
+      self.blur_err[i] = blur_est._estimate(X, 
+                                            y, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
+                                            model=model,
+                                            rand_type=rand_type,
+                                            est_risk=est_risk)
+
+      Z = model.get_membership_matrix(X)
+      y_fit = y if rand_type == 'full' else blur_est.w
+      self.test_err[i] = test_est._estimate(model=LinearRegression(),
+                                            X=Z,
+                                            y=y_fit,
+                                            y_test=y_test, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            est_risk=est_risk)
 
     return (self.test_err,
-            self.test_err_alpha,
-            self.cb_err,
+            # self.test_err_alpha,
+            # self.cb_err,
             self.blur_err)
 
   def compareBlurLinearIID(self, 
@@ -217,9 +206,9 @@ class ErrorComparer(object):
     self.cb_err = np.zeros(niter)
     self.blur_err = np.zeros(niter)
 
-    test_est = test_set_estimator
-    cb_est = cb
-    blur_est = blur_linear
+    test_est = TestSetEstimator()
+    cb_est = CB()
+    blur_est = BlurLinear()
 
     gen_beta = X is None or beta is None
 
@@ -247,30 +236,30 @@ class ErrorComparer(object):
       y_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
       y_test_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
 
-      self.test_err[i] = test_est(model=model,
-                                  X=X, 
-                                  y=y, 
-                                  y_test=y_test, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  est_risk=est_risk)[0]
-      self.test_err_alpha[i] = test_est(model=model,
-                                        X=X, 
-                                        y=y_alpha, 
-                                        y_test=y_test_alpha, 
-                                        Chol_t=np.eye(n)*np.sqrt(1+alpha)*sigma, 
-                                        est_risk=est_risk)[0]
-      self.cb_err[i] = cb_est(X=X, 
-                              y=y, 
-                              Chol_t=np.eye(n)*sigma, 
-                              Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                              model=model,
-                              est_risk=est_risk)[0]
-      self.blur_err[i] = blur_est(X, 
-                                  y, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                                  model=model,
-                                  est_risk=est_risk)[0]
+      self.test_err[i] = test_est._estimate(model=model,
+                                            X=X, 
+                                            y=y, 
+                                            y_test=y_test, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            est_risk=est_risk)
+      self.test_err_alpha[i] = test_est._estimate(model=model,
+                                                  X=X, 
+                                                  y=y_alpha, 
+                                                  y_test=y_test_alpha, 
+                                                  Chol_t=np.eye(n)*np.sqrt(1+alpha)*sigma, 
+                                                  est_risk=est_risk)
+      self.cb_err[i] = cb_est._estimate(X=X, 
+                                        y=y, 
+                                        Chol_t=np.eye(n)*sigma, 
+                                        Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
+                                        model=model,
+                                        est_risk=est_risk)
+      self.blur_err[i] = blur_est._estimate(X, 
+                                            y, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
+                                            model=model,
+                                            est_risk=est_risk)
 
     return (self.test_err,
             self.test_err_alpha,
@@ -286,6 +275,7 @@ class ErrorComparer(object):
                            X=None,
                            beta=None,
                            model=RelaxedLasso(),
+                           rand_type='full',
                            alpha=0.05,
                            est_risk=True):
 
@@ -294,9 +284,9 @@ class ErrorComparer(object):
     self.cb_err = np.zeros(niter)
     self.blur_err = np.zeros(niter)
 
-    test_est = test_set_estimator
-    # cb_est = cb
-    blur_est = blur_lasso
+    test_est = TestSetEstimator()
+    # cb_est = CB()
+    blur_est = BlurLasso()
 
     gen_beta = X is None or beta is None
 
@@ -324,27 +314,28 @@ class ErrorComparer(object):
       # y_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
       # y_test_alpha = mu + sigma * np.sqrt(1 + alpha) * np.random.randn(n)
 
-      # self.cb_err[i] = cb_est(X=X, 
+      # self.cb_err[i] = cb_est._estimate(X=X, 
       #                                   y=y, 
       #                                   Chol_t=np.eye(n)*sigma, 
       #                                   Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
       #                                   model=model,
-      #                                   est_risk=est_risk)[0]
-      (self.blur_err[i],
-       fitted_model) = blur_est(X, 
-                                y, 
-                                Chol_t=np.eye(n)*sigma, 
-                                Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
-                                model=model,
-                                est_risk=est_risk)
+      #                                   est_risk=est_risk)
+      self.blur_err[i] = blur_est._estimate(X, 
+                                            y, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            Chol_eps=np.eye(n)*np.sqrt(alpha)*sigma,
+                                            model=model,
+                                            rand_type=rand_type,
+                                            est_risk=est_risk)
 
-      XE = X[:, fitted_model.E_] if fitted_model.E_.shape[0] != 0 else np.zeros((X.shape[0],1))
-      self.test_err[i] = test_est(model=LinearRegression(),
-                                  X=XE, 
-                                  y=y, 
-                                  y_test=y_test, 
-                                  Chol_t=np.eye(n)*sigma, 
-                                  est_risk=est_risk)[0]
+      XE = X[:, model.E_] if model.E_.shape[0] != 0 else np.zeros((X.shape[0],1))
+      y_fit = y if rand_type == 'full' else blur_est.w
+      self.test_err[i] = test_est._estimate(model=LinearRegression(),
+                                            X=XE, 
+                                            y=y_fit, 
+                                            y_test=y_test, 
+                                            Chol_t=np.eye(n)*sigma, 
+                                            est_risk=est_risk)
 
     return (self.test_err,
             # self.cb_err,
