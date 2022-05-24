f9cab735c6566dea38b3e0130f3dfcb1c32af2c2
kevinbfry <15952367+kevinbfry@users.noreply.github.com>
2022-05-24 10:36:14 -0400

    smoke tests

--- spe/tests/test_linear.py
+++ spe/tests/test_linear.py
@@ -1,7 +1,7 @@
 import numpy as np
 from spe.mse_estimator import ErrorComparer
 
-def test_relaxed_lasso(n=100): 
+def test_linear(n=100): 
     nx = ny = int(np.sqrt(n))
     xs = np.linspace(0, 10, nx)
     ys = np.linspace(0, 10, ny)
@@ -15,7 +15,7 @@ def test_relaxed_lasso(n=100):
     (true_mse_tst,
      kfcv_mse_tst, 
      spcv_mse_tst,
-     gmcp_mse_tst) = mse_sim.compareRelaxedLassoTrTs(
+     gmcp_mse_tst) = mse_sim.compareLinearTrTs(
                                         niter=10,
                                         n=n,
                                         p=30,
@@ -26,8 +26,7 @@ def test_relaxed_lasso(n=100):
                                         coord=coord,
                                         Chol_t=None,
                                         Chol_s=None,
-                                        alpha=1.,
-                                        lambd=0.31,
                                         tr_idx=None,
                                         k=10,
                                         )
+
--- spe/tests/test_rf.py
+++ spe/tests/test_rf.py
@@ -1,33 +1,34 @@
 import numpy as np
 from spe.mse_estimator import ErrorComparer
 
-def test_relaxed_lasso(n=100): 
+def test_rf(n=100):
     nx = ny = int(np.sqrt(n))
     xs = np.linspace(0, 10, nx)
     ys = np.linspace(0, 10, ny)
     c_x, c_y = np.meshgrid(xs, ys)
     c_x = c_x.flatten()
     c_y = c_y.flatten()
-    coord = np.stack([c_x, c_y]).T
+    coord = np.stack([c_x, c_y]).T 
 
     mse_sim = ErrorComparer()
 
     (true_mse_tst,
      kfcv_mse_tst, 
      spcv_mse_tst,
-     gmcp_mse_tst) = mse_sim.compareRelaxedLassoTrTs(
+     gmcp_mse_tst) = mse_sim.compareForestTrTs(
                                         niter=10,
                                         n=n,
                                         p=30,
-                                        s=5,
+                                        s=30,
                                         snr=0.4, 
                                         X=None,
                                         beta=None,
                                         coord=coord,
                                         Chol_t=None,
                                         Chol_s=None,
-                                        alpha=1.,
-                                        lambd=0.31,
+                                        max_depth=4,
+                                        n_estimators=5,
                                         tr_idx=None,
                                         k=10,
+                                        bootstrap_type='blur',
                                         )
